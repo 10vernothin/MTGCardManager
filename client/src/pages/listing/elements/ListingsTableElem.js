869e5cea1fa989d237e2e3ceca9ce7f5450a1b99
Andy Rei Kou <AL_isakira@hotmail.com>
2020-04-14 20:06:27 -0400

    v2.3 part 2 -- Starting on CSS splitting, class restructuring, updating version.txt

--- client/src/pages/listing/elements/ListingsTableElem.js
+++ client/src/pages/listing/elements/ListingsTableElem.js
@@ -14,17 +14,53 @@ class ListingsTableElement extends Component {
       }
     }
   
-    /*
-      This function fetches the row data,
-      then calls fetchImage to retrieve the cached image url from the callback,
-      and finally sets the whole state
-    */
-    recordChange = () => {
+    render() {
+      this.loadChanges();
+      return(this.renderTableElem())
+    }
+
+    //Render Functions
+    
+    renderTableElem = () => {
+      let item = this.state.item;
+      if (!(item.name === '')){
+        return(
+          <div class={this.props.currCSS}>
+              <div style={{flex: 2}} class='shrunk_preview_panel'>
+                <CardImagePanel
+                  cardObj={this.state.cardObj}
+                  imgType={{type:'art_crop'}}
+                />
+              </div>
+              <div style={{flex: 2}}>{item.name}</div>
+              <div style={{flex: 7}}>{item.description}</div>
+              <div style={{flex: 1}}>${item.sum}</div>
+              <div style={{flex: 2}}>
+                    <SelectCollectionButton link_url={this.props.link_url}/>
+                    <EditCollectionButton link_url={this.props.edit_url}/>
+                    <DeleteCollectionButton col_id={item.id} updateTopmostState={this.props.updateTopmostState}/>
+              </div>
+              <div style={{flex: 1}}>
+                <button onClick={this.handleJSONDownload}>As JSON</button>
+                </div>
+              <div style={{flex: 1}}>
+                <button onClick={this.handleCSVDownload}>As CSV</button>
+              </div>
+          </div>
+        )
+        } else {
+          return(null);
+      }
+    }
+
+    //Loader Functions
+
+    loadChanges = () => {
       if (!(JSON.stringify(this.state.item) === JSON.stringify(this.props.item))) {
           if (this.props.item.showcase_card_id === 0) {
             this.setState({item: this.props.item})
           } else {
-            this.fetchCardObj(this.props.item.showcase_card_id, (obj) => {
+            this.loadCardObj(this.props.item.showcase_card_id, (obj) => {
                 this.setState({
                 item: this.props.item, 
                 cardObj: obj})
@@ -33,7 +69,7 @@ class ListingsTableElement extends Component {
       }
     }
 
-    fetchCardObj = (id, callback) => {
+    loadCardObj = (id, callback) => {
       fetch('/api/collections/fetch-row', {
         method: 'POST', 
         headers: { 'Content-Type': 'application/json'},
@@ -50,7 +86,9 @@ class ListingsTableElement extends Component {
       })
     }
 
-    downloadJSON = () => {
+    //Handler Methods
+
+    handleJSONDownload = () => {
       fetch('/api/collections/fetch-collection-as-JSON', {
         method: 'POST', 
         headers: { 'Content-Type': 'application/json'},
@@ -68,7 +106,7 @@ class ListingsTableElement extends Component {
       })
     }
 
-    downloadCSV = () => {
+    handleCSVDownload = () => {
       fetch('/api/collections/fetch-collection-as-CSV', {
         method: 'POST', 
         headers: { 'Content-Type': 'application/json'},
@@ -85,39 +123,7 @@ class ListingsTableElement extends Component {
           })
       })
     }
-
-    render(){
-      this.recordChange();
-      let item = this.state.item;
-      if (!(item.name === '')){
-        return(
-          <div style={this.props.currCSS}>
-              <div style={{flex: 2, width: '150px', height: '110px', border: '1px black solid', padding:'0', margin:'0'}}>
-                <CardImagePanel
-                  cardObj={this.state.cardObj}
-                  imgType={{type:'art_crop'}}
-                />
-              </div>
-              <div style={{flex: 2}}>{item.name}</div>
-              <div style={{flex: 7}}>{item.description}</div>
-              <div style={{flex: 1}}>${item.sum}</div>
-              <div style={{flex: 2}}>
-                    <SelectCollectionButton link_url={this.props.link_url}/>
-                    <EditCollectionButton link_url={this.props.edit_url}/>
-                    <DeleteCollectionButton col_id={item.id} updateTopmostState={this.props.updateTopmostState}/>
-              </div>
-              <div style={{flex: 1}}>
-                <button onClick={this.downloadJSON}>As JSON</button>
-                </div>
-              <div style={{flex: 1}}>
-                <button onClick={this.downloadCSV}>As CSV</button>
-              </div>
-          </div>
-        )
-        } else {
-          return(null);
-      }
-    }
+    
 }
 
 export default ListingsTableElement;
